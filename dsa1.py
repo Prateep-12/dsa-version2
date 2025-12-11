af7b6b5547fb6816371117cf2d50ec219e1d2965
Prateep-12 <prateepsahu123@gmail.com>
2025-12-11 14:26:33 +0530

    if else hai

--- dsa1.py
+++ dsa1.py
@@ -1,22 +1,8 @@
-class Solution:
-    def studentGrade(self, marks):
-        if marks >= 90:
-            return "Grade A"
-        elif marks >= 70:
-            return "Grade B"
-        elif marks >= 50:
-            return "Grade C"
-        elif marks >= 35:
-            return "Grade D"
-        else:
-            return "Fail"
 
-if __name__ == "__main__":
-    # 1. Create the object
-    solution = Solution()
-    
-    # 2. Read the number from input.txt (Does NOT print it)
-    marks = int(input())
-    
-    # 3. Calculate and Print ONLY the grade
-    print(solution.studentGrade(marks))
+# Just write the logic directly!
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A")
+else:
+    print("Fail")
